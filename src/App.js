f490e79547f821d413c1cdbf4b19a2014946a308
Elias <eliasker@gmail.com>
2020-08-11 17:40:28 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const App = () => {
+  return (
+    <div>
+      <p>Hello world</p>
+    </div>
+  )
+}
+
+export default App
