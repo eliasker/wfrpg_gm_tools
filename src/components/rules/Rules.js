fdacb8ee060cf80d4f6c4079a17d78b744f5523d
eliasker <eliasker@gmail.com>
2020-08-22 02:36:16 +0300

    dynamic content list, added TODOs

--- src/components/rules/Rules.js
+++ src/components/rules/Rules.js
@@ -5,31 +5,34 @@ import Talents from './Talents'
 import Skills from './Skills'
 import Spells from './Spells'
 
+// TODO: Rename this component
 const Rules = () => {
   const [current, showCurrent] = useContent()
 
+  // TODO: Move this elsewhere
   const Combat = () => (
     <HitLocation />
   )
 
+  const options = ['Combat', 'Talents', 'Skills', 'Spells', 'Prayers']
+
   const getCurrentContent = () => {
     switch (current) {
-      case 'combat': return <Combat />
-      case 'talents': return <Talents />
-      case 'skills': return <Skills />
-      case 'spells': return <Spells />
-      case 'prayers': return <p>Prayers todo</p>
+      case 'Combat': return <Combat />
+      case 'Talents': return <Talents />
+      case 'Skills': return <Skills />
+      case 'Spells': return <Spells />
+      case 'Prayers': return <p>Prayers todo</p>
       default: return null
     }
   }
+
+  const mapOptions = () => options.map(o => <li className='pointer' onClick={() => showCurrent(o)} key={o}>{o}</li>)
+
   return (
     <div>
       <ul>
-        <li className='pointer' onClick={() => showCurrent('combat')}>Combat</li>
-        <li className='pointer' onClick={() => showCurrent('talents')}>Talents</li>
-        <li className='pointer' onClick={() => showCurrent('skills')}>Skills</li>
-        <li className='pointer' onClick={() => showCurrent('spells')}>Spells</li>
-        <li className='pointer' onClick={() => showCurrent('prayers')}>Prayers</li>
+        {mapOptions()}
       </ul>
       <div>
         {getCurrentContent()}
